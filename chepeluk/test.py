c7c471d64ebe4da68729f4fe372bc264f8949d9b
Dmitry Kozerovsky <dima29koz@yandex.ru>
2022-10-10 18:48:46 +0300

    added task6

---
+++ chepeluk/test.py
@@ -0,0 +1,26 @@
+import matplotlib
+
+matplotlib.use('TkAgg')
+
+import seaborn as seaborn
+import matplotlib.pyplot as plot
+import numpy as np
+
+seaborn.set_style("darkgrid")
+
+mean = 3
+number =50
+
+x1 = np.random.normal(mean, 1, size=number)
+y1 = np.random.normal(mean, 1, size=number)
+z1 = np.random.normal(mean, 1, size=number)
+
+plot.figure(figsize=(6,5))
+axes = plot.axes(projection='3d')
+print(type(axes))
+axes.scatter3D(x1, y1, z1)
+
+axes.set_xlabel('x')
+axes.set_ylabel('y')
+axes.set_zlabel('z')
+plot.show()
